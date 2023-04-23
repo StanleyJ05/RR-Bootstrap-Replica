1717b18776f6e67baabb1b5192df1d9397abf426
Stanley Jackson <stanleyjackson11.sj@gmail.com>
2023-04-23 07:19:31 -0700

    changed font size on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import TopBar from './components/TopBar';
+import styled from 'styled-components';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container  from 'react-bootstrap/Container';
+import StoreListing from './components/StoreListing';
+import splashImage from './splash.webp';
 
+const Splash = styled.div`
+ width: 100vw;
+ height: 500px;
+ background-repeat: no-repeat;
+ background-size: cover;
+ background-image: url(${splashImage});
+ background-color: #dfeed6;
+ position: absolute;
+ top: 40px;
+  left: 0;
+ right: 0;
+ z-index: -1;
+
+`
+const StyledStoreListing = styled(StoreListing)`
+position: relative;
+top:600px;
+`
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <TopBar/>
+    <StyledStoreListing />
+    <Splash/>
+    </>
   );
 }
 
